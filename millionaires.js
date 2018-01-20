00959f9b165991b37bf4b0a62968d2354c9112a9
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-01-20 23:23:58 +0100

    Fix the show up second question

--- millionaires.js
+++ millionaires.js
@@ -1,153 +1,148 @@
 var beginGame = document.getElementById('start');
 var allQuestions = {
     "Questions": [{
-        "QuestionTitle": "Jak się nazywa twoj przyszly pracownik?",
-        "answers": [{
-            "id": "A",
-            "answerTitle": "Daniel Lepszy",
-            "isCorrectAnswer": true
+            "QuestionTitle": "Jak się nazywa twoj przyszly pracownik?",
+            "answers": [{
+                    "id": "A",
+                    "answerTitle": "Daniel Lepszy",
+                    "isCorrectAnswer": true
+                },
+                {
+                    "id": "B",
+                    "answerTitle": "Elvis Presley"
+                },
+                {
+                    "id": "C",
+                    "answerTitle": "Violetta Villas"
+                },
+                {
+                    "id": "D",
+                    "answerTitle": "Mark Zuckerberg"
+                }
+            ],
+            "correctAnswerId": "A"
         },
         {
-            "id": "B",
-            "answerTitle": "Elvis Presley"
+            "QuestionTitle": "Kto jest najlepszy?",
+            "answers": [{
+                    "id": "A",
+                    "answerTitle": "Daniel ",
+                    "isCorrectAnswer": true
+                },
+                {
+                    "id": "B",
+                    "answerTitle": "Elvis "
+                },
+                {
+                    "id": "C",
+                    "answerTitle": "Violetta "
+                },
+                {
+                    "id": "D",
+                    "answerTitle": "Mark "
+                }
+            ],
+            "correctAnswerId": "A"
         },
         {
-            "id": "C",
-            "answerTitle": "Violetta Villas"
-        },
-        {
-            "id": "D",
-            "answerTitle": "Mark Zuckerberg"
+            "QuestionTitle": "Kogo zaprosisz na rozmowe kwalifikacyjna?",
+            "answers": [{
+                    "id": "A",
+                    "answerTitle": "Daniel Lepsz",
+                    "isCorrectAnswer": true
+                },
+                {
+                    "id": "B",
+                    "answerTitle": "Elvis Presly"
+                },
+                {
+                    "id": "C",
+                    "answerTitle": "Violetta Villa"
+                },
+                {
+                    "id": "D",
+                    "answerTitle": "Mark Zuckerbe"
+                }
+            ],
+            "correctAnswerId": "A"
         }
-        ],
-        "correctAnswerId": "A"
-    },
-    {
-        "QuestionTitle": "Kto jest najlepszy?",
-        "answers": [{
-            "id": "A",
-            "answerTitle": "Daniel ",
-            "isCorrectAnswer": true
-        },
-        {
-            "id": "B",
-            "answerTitle": "Elvis "
-        },
-        {
-            "id": "C",
-            "answerTitle": "Violetta "
-        },
-        {
-            "id": "D",
-            "answerTitle": "Mark "
-        }
-        ],
-        "correctAnswerId": "A"
-    },
-    {
-        "QuestionTitle": "Kogo zaprosisz na rozmowe kwalifikacyjna?",
-        "answers": [{
-            "id": "A",
-            "answerTitle": "Daniel Lepsz",
-            "isCorrectAnswer": true
-        },
-        {
-            "id": "B",
-            "answerTitle": "Elvis Presly"
-        },
-        {
-            "id": "C",
-            "answerTitle": "Violetta Villa"
-        },
-        {
-            "id": "D",
-            "answerTitle": "Mark Zuckerbe"
-        }
-        ],
-        "correctAnswerId": "A"
-    }
     ]
 }
 var boxOfQuestions = [];
 var boxOfAnswers = [];
 var clearAnswer = document.getElementById('allAnswers');
 
-var firstAnswer = document.getElementById('answerA');
-var secondAnswer = document.getElementById('answerB');
-var thirdAnswer = document.getElementById('answerC');
-var fourthAnswer = document.getElementById('answerD');
+beginGame.addEventListener("click", function () {
+    firstMusic()
+    focusMusic(6400)
+    getQuestions(0)
+    getAnswers(0)
+    showSet(0, 6400)
+});
 
-console.log(secondAnswer);
-beginGame.addEventListener("click", function () { firstMusic(), focusMusic(6400), getQuestions(0), getAnswers(0), showSet(0,6400) });
-$('body').one('click', '#allAnswers', function () { checkAnswer(),
-     getQuestions(1), getAnswers(1),focusMusic(18400),showSet(1,18400),checkAnswer() }); //Zdarzenie oCLICK na obiekcie jQuery
+$('body').one('click', '#allAnswers', function () {
+    checkAnswer()
+    getQuestions(1)
+    getAnswers(1)
+    focusMusic(18400)
+    showSet(0, 18400)
+    
+}); //Zdarzenie oCLICK na obiekcie jQuery
 
 
 function firstMusic() {
-    $('#audio').delay(6400).attr('src', 'Sounds/Begin.mp3');
+    $('#audio').attr('src', 'Sounds/Begin.mp3');
 };
 
 function focusMusic(time) {
-
     setTimeout(function () {
         $('#audio').attr('src', 'Sounds/FocusSound.mp3');
     }, time);
 };
 
-
 function getQuestions(y) {
-
-    
     boxOfQuestions.push(allQuestions.Questions[y].QuestionTitle);
-    return boxOfQuestions;
-
-}
+};
 
 function getAnswers(x) {
-   var boxOfAnswers2 = [];
     for (var j = 0; j <= 3; j++) {
-
         boxOfAnswers.push(allQuestions.Questions[x].answers[j].answerTitle);
     }
-    return boxOfAnswers;
-}
+};
 
-function showSet(z,time) {
-
+function showSet(z, time) {
     setTimeout(function () {
-
-         $('#mainQuestion p').after('<p>' +  boxOfQuestions[z] + '</p>');
-        setTimeout(function () { $('#answerA .firstRowAnswer').after('<p>' + boxOfAnswers[0] + '</p>') }, 2000);
-        setTimeout(function () { $('#answerB .firstRowAnswer').after('<p>' + boxOfAnswers[1] + '</p>') }, 4000);
-        setTimeout(function () { $('#answerC .firstRowAnswer').after('<p>' + boxOfAnswers[2] + '</p>') }, 6000);
-        setTimeout(function () { $('#answerD .firstRowAnswer').after('<p>' + boxOfAnswers[3] + '</p>') }, 8000);
-
-       // $("#answerA .firstRowAnswer").css({
-            
-      //                 'z-index': '2'
-      //  });
+        $('#mainQuestion').html('<p>' + boxOfQuestions[z] + '</p>');
+        setTimeout(function () {
+            $('#answerA .firstRowAnswer').after('<p>' + boxOfAnswers[0] + '</p>')
+        }, 2000);
+        setTimeout(function () {
+            $('#answerB .firstRowAnswer').after('<p>' + boxOfAnswers[1] + '</p>')
+        }, 4000);
+        setTimeout(function () {
+            $('#answerC .firstRowAnswer').after('<p>' + boxOfAnswers[2] + '</p>')
+        }, 6000);
+        setTimeout(function () {
+            $('#answerD .firstRowAnswer').after('<p>' + boxOfAnswers[3] + '</p>')
+        }, 8000);
     }, time);
-
-}
     
+}
+
 function checkAnswer() {
-
-
+    boxOfQuestions = [];
+    boxOfAnswers = [];
     $('#audio').attr('src', 'Sounds/GoodAnswers.mp3');
 
     $("#answerA").delay(100).animate({
-
         backgroundColor: '#FFCF40',
         opacity: 1,
         color: '000000',
-    },
-        500);
+    }, 500);
     $("#answerA").delay(9400).animate({
-
         backgroundColor: '#23E047',
+    }, 500);
 
-    },
-        500);
     setTimeout(function () {
         $('#mainQuestion p').empty();
         $('#answerA p:nth-child(2)').remove();
@@ -156,25 +151,16 @@ function checkAnswer() {
         $('#answerD p:nth-child(2)').remove();
 
         $("#answerA").delay(9400).animate({
-
-            'backgroundColor': 'black'
-        },
-        
-            0);
-        
-        
+            'backgroundColor': 'black',
+            'color': 'white'
+        }, 0);
     }, 15000);
-    
+
 }
 
-    
+
 /*function clearBoxes(){
      boxOfQuestions.shift();
      
      return boxOfQuestions.shift();
 } */
-
-
-
-
-
