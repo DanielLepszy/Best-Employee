7e2634fb526e6ee8b3a7442440a022fbf0e5e8af
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-01-28 21:37:35 +0100

    Mix the question in JSON file

--- millionaires.js
+++ millionaires.js
@@ -8,6 +8,7 @@ var clearAnswer = document.getElementById('allAnswers');
 var currentQuestion;
 function loadJSON() {
     var value = $.ajax({
+        
         url: 'https://raw.githubusercontent.com/DanielLepszy/Best-Employee/master/questions_and_answers.json',
         async: false // 
     }).responseText;
